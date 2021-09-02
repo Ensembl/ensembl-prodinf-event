82f0fc8cbafa357785444b2be782001427f138ec
vinay <vinay@ebi.ac.uk>
2021-09-02 12:15:03 +0100

    new feature to lauch job async remotely; new methods to get pysaga job status

--- src/ensembl/production/workflow/monitor.py
+++ src/ensembl/production/workflow/monitor.py
@@ -4,7 +4,7 @@ import json
 import re
 import time
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, func
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, VARCHAR, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 __all__ = ['RemoteCmd']
@@ -23,24 +23,31 @@ class Beekeeper(Base):
         return "<beekeeper(beekeeper_id='%s', cause_of_death='%s')>" % (
             self.beekeeper_id, self.cause_of_death)
 
+class Worker(Base):
+    __tablename__ = 'worker'
+
+    worker_id  = Column(Integer, primary_key=True)
+    process_id = Column(VARCHAR)
+    cause_of_death = Column(VARCHAR)
+
+    def __repr__(self):
+        return "<beekeeper(worker_id='%s', process_id='%s')>" % (
+            self.worker_id, self.process_id)
 
 class RemoteCmd():
 
     def __init__(self, **args):
-        self.REMOTE_HOST = args.get('remote_host', "noah-login-01")
-        self.ADDRESS = args.get('address', '10.7.95.60')  # Address of your server
-        self.USER = args.get('user', 'vinay')  # Username
-        self.PASSWORD = args.get('pass', '')  # That's amazing I got the same combination on my luggage!
-        self.WORKING_DIR = args.get('pwd', '/homes/ensprod/vinay_test/covid')  # Your working directory
+        self.REMOTE_HOST = args.get('REMOTE_HOST', None)
+        self.ADDRESS = args.get('ADDRESS', None)  # Address of your server
+        self.USER = args.get('USER', 'vinay')  # Username
+        self.PASSWORD = args.get('PASSWORD', '')  # That's amazing I got the same combination on my luggage!
+        self.WORKING_DIR = args.get('WORKING_DIR', None)  # Your working directory
         self.mysql_url = args.get('mysql_url', None)  # hive database string
         self.ctx = saga.Context("ssh")
         self.ctx.user_id = self.USER
         self.session = saga.Session()
         self.session.add_context(self.ctx)
-        # hive instance
-        self.hive_session = sessionmaker()
-        self.engine = create_engine(self.mysql_url, pool_recycle=3600, echo=False)
-        self.hive_session.configure(bind=self.engine)
+
 
     def run_job(self, **args):
         """
@@ -55,36 +62,68 @@ class RemoteCmd():
             jd.error = args.get("stderr", 'stderr.log')
             jd.working_directory = args.get('pwd', self.WORKING_DIR)
             js = saga.job.Service('ssh://' + self.ADDRESS, session=self.session)
+            job_synchronus = args.get('synchronus', False)
             myjob = js.create_job(jd)
             print("\n...starting job...\n")
-            print(args['args'])
             # Now we can start our job.
             myjob.run()
             print("Job ID    : %s" % (myjob.id))
             print("Job State : %s" % (myjob.state))
+            if job_synchronus:
+                print("\n...waiting for job to complete...\n")
+                # wait for the job to either finish or fail
+                myjob.wait()
+                print("Job State : %s" % (myjob.state))
+                print("Exitcode  : %s" % (myjob.exit_code))
+                if myjob.exit_code != 0:
+                    return {'status': False, 'state': myjob.state,
+                            'error': 'Failed to run the job check stderr:' + jd.working_directory}
 
-            print("\n...waiting for job...\n")
-            # wait for the job to either finish or fail
-            myjob.wait()
-            print("Job State : %s" % (myjob.state))
-            print("Exitcode  : %s" % (myjob.exit_code))
-            if myjob.exit_code != 0:
-                return {'status': False, 'state': myjob.state,
-                        'error': 'Failed to run the job check stderr:' + jd.working_directory}
+            return {'status': True, 'error': '', 'state': myjob.state, 'job_id': myjob.id }
+        except Exception as e:
+            print(str(e))
+            return {'status': False, 'error': str(e)}
 
-            return {'status': True, 'error': '', 'state': myjob.state}
+
+    def job_status(self, job_id, stop_job=False):
+        """
+        Get the job status / Stop the submitted job 
+        """
+        try:        
+
+            service = saga.job.Service('ssh://' + self.ADDRESS, session=self.session)
+            #stop the running jobs
+            job = service.get_job(job_id)
+            if stop_job:
+                if job.get_state() in  [ saga.job.PENDING, saga.job.RUNNING ]:
+                    job.cancel()
+                else:
+                    raise ValueError('Job already stopped')
+
+            return {'status': True, 'error': '', 'job_status': job.get_state() }
+
         except Exception as e:
             return {'status': False, 'error': str(e)}
 
-    def beekeper_status(self):
+    def beekeper_status(self, mysql_url=None):
         """
         Check beekeeper executed all the jobs
         """
-        s = self.hive_session()
         try:
+
+            engine = create_engine(mysql_url, pool_recycle=3600, echo=False)
+            hive_session = sessionmaker()
+            hive_session.configure(bind=engine)  
+            s = hive_session()  
             result = s.query(Beekeeper).order_by(Beekeeper.beekeeper_id.desc()).first()
             if (result == None):
-                return {'status': False, 'error': 'No Records', 'value': ''}
+                raise ValueError('No Records')
+            
             return {'status': True, 'error': '', 'value': result.cause_of_death}
-        finally:
-            s.close()
+
+        except  Exception as e:
+            return {'status': False, 'error': str(e), 'value': ''}
+
+            
+    
+  
