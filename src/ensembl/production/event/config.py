a42f603e96c9302bf9500c943fce0fc58a50564c
vinay <vinay@ebi.ac.uk>
2021-12-03 06:07:50 +0000

    automerge changes

--- src/ensembl/production/event/config.py
+++ src/ensembl/production/event/config.py
@@ -10,13 +10,13 @@ class config():
                                                                             'event_config.dev.yaml'))
         file_config = load_config_yaml(config_file_path)
         report_server = os.environ.get("REPORT_SERVER",
-                                file_config.get('report_server', "amqp://guest:guest@localhost:5672/"))
+                                file_config.get('report_server', "amqp://guest:guest@ensrabbitmq:5672/"))
         report_exchange = os.environ.get("REPORT_EXCHANGE",
                                  file_config.get('report_exchange', 'event_report_exchange'))
         report_exchange_type = os.environ.get("REPORT_EXCHANGE_TYPE",
                                       file_config.get('report_exchange_type', 'topic'))                                 
         event_uri = os.environ.get("EVENT_URI",
-                                   file_config.get('event_uri', 'http://localhost:5000/'))
+                                   file_config.get('event_uri', 'http://event_app:5000/'))
 
 
 class EventConfig(config):
@@ -29,10 +29,10 @@ class EventConfig(config):
                                 config.file_config.get('process_lookup_file', 
                                 os.path.join(os.path.dirname(__file__), "./process_lookup.json")))
 
-        # hive_url = os.environ.get("HIVE_URL",config.file_config.get('hive_url', ''))  
-        # farm_user = os.environ.get("FARM_USER",config.file_config.get('user', ''))  
-        ES_HOST = os.environ.get('ES_HOST', config.file_config.get('es_host', ''))
-        ES_PORT = os.environ.get('ES_PORT', config.file_config.get('es_port', ''))
+        # hive_url = os.environ.get("HIVE_URL",config.file_config.get('hive_url', 'mysql://ensadmin:ensembl@mysql-ens-hive-prod-1:4575/'))  
+        # farm_user = os.environ.get("FARM_USER",config.file_config.get('user', 'vinay'))  
+        ES_HOST = os.environ.get('ES_HOST', config.file_config.get('es_host', 'elasticsearch'))
+        ES_PORT = os.environ.get('ES_PORT', config.file_config.get('es_port', '9200'))
         ES_INDEX = os.environ.get('ES_INDEX', config.file_config.get('es_index', 'reports_workflow'))
         RELEASE = os.environ.get('ENS_RELEASE', config.file_config.get('ens_release', '105'))
         EG_RELEASE = os.environ.get('EG_RELEASE', config.file_config.get('eg_release', '52'))
@@ -42,9 +42,9 @@ class EventCeleryConfig(config):
         """ Config For Celery App"""
 
         broker_url = os.environ.get("CELERY_BROKER_URL",
-                            config.file_config.get('celery_broker_url', 'pyamqp://guest:guest@localhost:5672/'))
+                            config.file_config.get('celery_broker_url', 'pyamqp://guest:guest@ensrabbitmq:5672/'))
         result_backend = os.environ.get("CELERY_RESULT_BACKEND",
-                                config.file_config.get('celery_result_backend', 'rpc://guest:guest@localhost:5672/'))
+                                config.file_config.get('celery_result_backend', 'rpc://guest:guest@ensrabbitmq:5672/'))
         smtp_server = os.environ.get("SMTP_SERVER",
                              config.file_config.get('smtp_server', 'localhost'))
         from_email_address = os.environ.get("FROM_EMAIL_ADDRESS",
@@ -64,20 +64,22 @@ class EventCeleryConfig(config):
 class PySagaConfig(config):
 
         NOAH = {
-          'REMOTE_HOST': os.environ.get("REMOTE_HOST_NOAH", config.file_config.get("remote_host_noah", "")), 
-          'ADDRESS' : os.environ.get("ADDRESS_NOAH", config.file_config.get("address_noah","")),
+          'REMOTE_HOST': os.environ.get("REMOTE_HOST_NOAH", config.file_config.get("remote_host_noah", "noah-login-01")), 
+          #'ADDRESS' : os.environ.get("ADDRESS_NOAH", config.file_config.get("address_noah","10.7.95.60")),
+          'ADDRESS' : os.environ.get("ADDRESS_NOAH", config.file_config.get("address_noah","localhost")),
           'USER' :  os.environ.get("USER", config.file_config.get("user","vinay")),  # vaild user in remote host 
           'PASSWORD' : os.environ.get("PASSWORD", ""),  # required only if ssh is not configured for remote user 
-          'WORKING_DIR' : os.environ.get('WORKING_DIR', config.file_config.get("pwd", "/homes/vinay/new_rapid_test"))  # Your working directory to store logs and temp dirs
+          'WORKING_DIR' : os.environ.get('WORKING_DIR', config.file_config.get("pwd", "/home/vinay/Documents/Ensembl-Master/Projects/ensembl_devlopemnt/refactoring/event/logs/"))  # Your working directory to store logs and temp dirs
         }
         CODON = {
-          'REMOTE_HOST': os.environ.get("REMOTE_HOST_CODON", config.file_config.get("remote_host_codon", "")), 
-          'ADDRESS' : os.environ.get("ADDRESS_NOAH", config.file_config.get("address_noah","")),
-          'USER' :  os.environ.get("USER", config.file_config.get("user","vinay")),  # vaild user in remote host 
+          'REMOTE_HOST': os.environ.get("REMOTE_HOST_CODON", config.file_config.get("remote_host_codon", "localhost")), 
+          #'ADDRESS' : os.environ.get("ADDRESS_CODON", config.file_config.get("address_codon","10.36.17.176")),
+          'ADDRESS' : os.environ.get("ADDRESS_CODON", config.file_config.get("address_codon","localhost")),
+          'USER' :  os.environ.get("USER", config.file_config.get("user","ubuntu")),  # vaild user in remote host 
           'PASSWORD' : os.environ.get("PASSWORD", ""),  # required only if ssh is not configured for remote user 
-          'WORKING_DIR' : os.environ.get('WORKING_DIR', config.file_config.get("pwd", "/homes/vinay/new_rapid_test"))  # Your working directory to store logs and temp dirs
+          'WORKING_DIR' : os.environ.get('WORKING_DIR', config.file_config.get("pwd", "/home/ubuntu/logs"))  # Your working directory to store logs and temp dirs
         }        
-        DEFAULT_HOST_DETAILS = os.environ.get("DEFAULT_HOST_DETAILS", config.file_config.get("default_host_details", ""))
-        FARM_USER = os.environ.get("FARM_USER",config.file_config.get('user', 'vinay'))
-        HIVE_URL = os.environ.get("HIVE_URL", config.file_config.get("hive_url", ''))  # hive database string
+        DEFAULT_HOST_DETAILS = os.environ.get("DEFAULT_HOST_DETAILS", config.file_config.get("default_host_details", "CODON"))
+        FARM_USER = os.environ.get("FARM_USER",config.file_config.get('user', 'ubuntu'))
+        HIVE_URL = os.environ.get("HIVE_URL", config.file_config.get("hive_url", 'mysql://root:root@mensmysql:3306/'))  # hive database string
 
