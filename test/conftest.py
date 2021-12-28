05e2e04f5c0ba00ce68083745e3f318b436201a8
vinay <vinay@ebi.ac.uk>
2021-12-28 14:23:22 +0000

    pydocker test dependency removed

--- test/conftest.py
+++ test/conftest.py
@@ -18,25 +18,6 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 
-# pytest_plugins = ["docker_compose"]
-
-# @pytest.fixture(scope="session")
-# def docker_compose_file(pytestconfig):
-#     return os.path.join(str(pytestconfig.rootdir), "docker-compose-test.yml")
-
-
-# @pytest.fixture(scope="module")
-# def event_app_url(module_scoped_container_getter):
-#     """ Wait for the event_app to become responsive """
-#     request_session = requests.Session()
-#     retries = Retry(total=5, backoff_factor=3, status_forcelist=[500, 502, 503, 504])
-#     request_session.mount("http://", HTTPAdapter(max_retries=retries))
-
-#     service = module_scoped_container_getter.get("event_app").network_info[0]
-#     api_url = f"http://{service.hostname}:{service.host_port}"
-#     return api_url
-
-
 
 @pytest.fixture
 def appclient():
